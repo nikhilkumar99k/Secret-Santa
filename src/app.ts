7d1b76fbfb785a3f760c4c2de3abf3418c0b70aa
Nikhil Kumar <guptanikku514@gmail.com>
2025-01-03 16:43:28 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import apiRoutes from './routers/base';
 
 const app: Application = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Load environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+// CORS Configuration - Allow all origins
+const corsOptions = {
+  origin: true, // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+};
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
 
 // Route handling
 app.use('/api', apiRoutes);
--- src/routers/base.ts
+++ src/routers/base.ts
@@ -8,5 +8,8 @@ const apiRouter: Router = express.Router();
 // Use the sub-routers
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/secret-santa', secterSantaRouter);
+apiRouter.get('/', (req, res) => {
+    res.send("<div><h1>Secret Santa API</h1></div>");
+})
 
 export default apiRouter;
